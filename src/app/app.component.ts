077b1fe508c8323a5fb1df89b7b799f40e491dfc
Guilherme Lima <gslima0806@gmail.com>
2024-10-09 22:09:24 -0300

    front-end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from "./componentes/container/container.component";
+import { AsideComponent } from "./componentes/aside/aside.component";
+import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContainerComponent, AsideComponent, CabecalhoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
